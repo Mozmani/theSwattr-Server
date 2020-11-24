20a3449c1d3c0a5c4ca516f0397f684a032604d0
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-11-24 09:56:11 -0700

    fixed validators

--- src/middlewares/body-validators.js
+++ src/middlewares/body-validators.js
@@ -13,150 +13,174 @@ function _errorResponse(res, undefField) {
 }
 
 const loginBody = (req, res, next) => {
-  const { user_name, password } = req.body;
-  const rawLogin = { user_name, password };
+  try {
+    const { user_name, password } = req.body;
+    const rawLogin = { user_name, password };
 
-  const undefField = checkFields(rawLogin);
-  if (undefField) {
-    _errorResponse(res, undefField);
-    return;
+    const undefField = checkFields(rawLogin);
+    if (undefField) {
+      _errorResponse(res, undefField);
+      return;
+    }
+
+    const loginUser = SerializeService.sanitizeObject(rawLogin);
+
+    req.loginUser = loginUser;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const loginUser = SerializeService.sanitizeObject(rawLogin);
-
-  req.loginUser = loginUser;
-  next();
 };
 
 const registrationBody = async (req, res, next) => {
-  const {
-    first_name,
-    last_name,
-    user_name,
-    password,
-    email,
-  } = req.body;
+  try {
+    const {
+      first_name,
+      last_name,
+      user_name,
+      password,
+      email,
+    } = req.body;
 
-  const rawUser = {
-    first_name,
-    last_name,
-    user_name,
-    password,
-    email,
-  };
+    const rawUser = {
+      first_name,
+      last_name,
+      user_name,
+      password,
+      email,
+    };
 
-  // ? for testing only
-  if (req.body.id) {
-    rawUser.id = req.body.id;
+    // ? for testing only
+    if (req.body.id) {
+      rawUser.id = req.body.id;
+    }
+
+    const undefField = checkFields(rawUser);
+    if (undefField) {
+      _errorResponse(res, undefField);
+      return;
+    }
+
+    const passError = validatePassword(rawUser.password);
+    if (passError) {
+      res.status(400).json({ error: passError });
+      return;
+    }
+
+    const newUser = SerializeService.sanitizeObject(rawUser);
+
+    const emailError = validateEmail(newUser.email);
+    if (emailError) {
+      res.status(400).json({ error: emailError });
+      return;
+    }
+
+    req.newUser = newUser;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const undefField = checkFields(rawUser);
-  if (undefField) {
-    _errorResponse(res, undefField);
-    return;
-  }
-
-  const passError = validatePassword(rawUser.password);
-  if (passError) {
-    res.status(400).json({ error: passError });
-    return;
-  }
-
-  const newUser = SerializeService.sanitizeObject(rawUser);
-
-  const emailError = validateEmail(newUser.email);
-  if (emailError) {
-    res.status(400).json({ error: emailError });
-    return;
-  }
-
-  req.newUser = newUser;
-  next();
 };
 
 const devBody = (req, res, next) => {
-  const { dev_secret } = req.body;
+  try {
+    const { dev_secret } = req.body;
 
-  if (!dev_secret) {
-    _errorResponse(res, 'dev_secret');
-    return;
+    if (!dev_secret) {
+      _errorResponse(res, 'dev_secret');
+      return;
+    }
+
+    if (dev_secret !== DEV_SECRET) {
+      res.status(401).json({ error: `Invalid code` });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  if (dev_secret !== DEV_SECRET) {
-    res.status(401).json({ error: `Invalid code` });
-    return;
-  }
-
-  next();
 };
 
 const bugBody = async (req, res, next) => {
-  const app = req.body.app.replace(/-/g, ' ');
-  const {
-    user_name,
-    bug_name,
-    description,
-    completed_notes,
-  } = req.body;
+  try {
+    const {
+      user_name,
+      bug_name,
+      description,
+      completed_notes,
+      app,
+    } = req.body;
 
-  const rawBug = { user_name, bug_name, description, app };
+    const rawBug = { user_name, bug_name, description, app };
 
-  // ? for testing only
-  if (req.body.id) {
-    rawBug.id = req.body.id;
+    // ? for testing only
+    if (req.body.id) {
+      rawBug.id = req.body.id;
+    }
+
+    const undefField = checkFields(rawBug);
+    if (undefField) {
+      _errorResponse(res, undefField);
+      return;
+    }
+
+    if (completed_notes) {
+      rawBug.completed_notes = completed_notes;
+    }
+
+    const newBug = SerializeService.sanitizeObject(rawBug);
+
+    req.newBug = newBug;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const undefField = checkFields(rawBug);
-  if (undefField) {
-    _errorResponse(res, undefField);
-    return;
-  }
-
-  if (completed_notes) {
-    rawBug.completed_notes = completed_notes;
-  }
-
-  const newBug = SerializeService.sanitizeObject(rawBug);
-
-  req.newBug = newBug;
-  next();
 };
 
 const linkageBody = async (req, res, next) => {
-  const { status, app, severity } = req.body;
-  const rawLinks = { status, app, severity };
+  try {
+    const { status, app, severity } = req.body;
+    const rawLinks = { status, app, severity };
 
-  const undefField = checkFields(rawLinks);
-  if (undefField) {
-    _errorResponse(res, undefField);
-    return;
+    const undefField = checkFields(rawLinks);
+    if (undefField) {
+      _errorResponse(res, undefField);
+      return;
+    }
+
+    const links = SerializeService.sanitizeObject(rawLinks);
+
+    req.links = links;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const links = SerializeService.sanitizeObject(rawLinks);
-
-  req.links = links;
-  next();
 };
 
 const commentBody = async (req, res, next) => {
-  const { bug_id, user_name, comment } = req.body;
-  const rawComment = { bug_id, user_name, comment };
+  try {
+    const { bug_id, user_name, comment } = req.body;
+    const rawComment = { bug_id, user_name, comment };
 
-  // ? for testing only
-  if (req.body.id) {
-    rawComment.id = req.body.id;
+    // ? for testing only
+    if (req.body.id) {
+      rawComment.id = req.body.id;
+    }
+
+    const undefField = checkFields(rawComment);
+    if (undefField) {
+      _errorResponse(res, undefField);
+      return;
+    }
+
+    const newComment = SerializeService.sanitizeObject(rawComment);
+
+    req.newComment = newComment;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const undefField = checkFields(rawComment);
-  if (undefField) {
-    _errorResponse(res, undefField);
-    return;
-  }
-
-  const newComment = SerializeService.sanitizeObject(rawComment);
-
-  req.newComment = newComment;
-  next();
 };
 
 module.exports = {
