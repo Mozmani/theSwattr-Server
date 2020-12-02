03fd6f369190b5642a3945c14f3f7bb03f05dd33
Mark <SwiftTactics@gmail.com>
2020-12-02 17:43:39 -0500

    comments tests working

--- tests/routes/app.test.js
+++ tests/routes/app.test.js
@@ -1,64 +1,61 @@
-const knex = require('knex');
+const knex = require("knex");
 
-const app = require('../../src/app');
-const helpers = require('../test-helpers');
-const { ROUTES } = require('../../src/constants/endpoints.constants');
-const { expect } = require('chai');
+const app = require("../../src/app");
+const helpers = require("../test-helpers");
+const { ROUTES } = require("../../src/constants/endpoints.constants");
+const { expect } = require("chai");
 
-describe('Route: App router', () => {
+describe("Route: App router", () => {
   const APP_EP = ROUTES.API + ROUTES.APP;
   const testDev = helpers.getSeedData().users_seed[0];
   const testUser = helpers.getSeedData().users_seed[1];
 
   let db;
-  before('make knex instance', () => {
+  before("make knex instance", () => {
     db = knex({
-      client: 'pg',
+      client: "pg",
       connection: TEST_DB_URL,
     });
-    app.set('db', db);
+    app.set("db", db);
   });
 
-  afterEach('cleanup', () => helpers.cleanTables(db));
+  afterEach("cleanup", () => helpers.cleanTables(db));
 
-  after('disconnect from db', () => db.destroy());
+  after("disconnect from db", () => db.destroy());
 
   const authHeaders = { dev: {}, nonDev: {} };
-  beforeEach('set auth headers', async () => {
+  beforeEach("set auth headers", async () => {
     await helpers.seedAllTables(db);
 
-    authHeaders.dev = await helpers.getAuthHeaders(
-      app,
-      testDev.user_name,
-      db,
-    );
+    authHeaders.dev = await helpers.getAuthHeaders(app, testDev.user_name, db);
     authHeaders.nonDev = await helpers.getAuthHeaders(
       app,
       testUser.user_name,
-      db,
+      db
     );
   });
 
-  it('rejects unauthorized user', () => {
+  it("rejects unauthorized user", () => {
     return supertest(app).get(APP_EP).expect(401);
   });
 
   describe(`ENDPOINT: '/app'`, () => {
-    context('GET', () => {
-      it('all formatted apps', () => {
-        const result = {        
+    context("GET", () => {
+      it("all formatted apps", () => {
+        const result = {
           apps: [
-            { id: 1, rawName: 'main-app', formatName: 'Main App' },
-            { id: 2, rawName: 'second-app', formatName: 'Second App' }
-          ]
-        }
-        
-        return supertest(app).get(APP_EP)
-        .set(authHeaders.dev)
-        .expect(200)
-        .expect(res => {
-          expect(res.body).to.deep.equal(result)
-        })
+            { id: 1, rawName: "main-app", formatName: "Main App" },
+            { id: 2, rawName: "second-app", formatName: "Second App" },
+          ],
+        };
+
+        return supertest(app)
+          .get(APP_EP)
+          .set(authHeaders.dev)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).to.deep.equal(result);
+          });
       });
     });
   });
--- tests/routes/comment.test.js
+++ tests/routes/comment.test.js
@@ -1,81 +1,265 @@
-const knex = require('knex');
+const knex = require("knex");
+const supertest = require("supertest");
+const { ROUTES } = require("../../src/constants/endpoints.constants");
+const app = require("../../src/app");
+const helpers = require("../test-helpers");
+const { expect } = require("chai");
 
-const app = require('../../src/app');
-const helpers = require('../test-helpers');
+describe("Route: Comment-Thread router", () => {
+  const COMMENTS_EP = ROUTES.API + ROUTES.COMMENT_THREAD;
+  const testDev = helpers.getSeedData().users_seed[0];
+  const testUser = helpers.getSeedData().users_seed[1];
 
-describe.skip('Route: Comment-Thread router', () => {
   let db;
-  before('make knex instance', () => {
+  before("make knex instance", () => {
     db = knex({
-      client: 'pg',
+      client: "pg",
       connection: TEST_DB_URL,
     });
-    app.set('db', db);
+    app.set("db", db);
   });
 
-  afterEach('cleanup', () => helpers.cleanTables(db));
+  afterEach("cleanup", () => helpers.cleanTables(db));
 
-  after('disconnect from db', () => db.destroy());
+  after("disconnect from db", () => db.destroy());
 
   const seedAllTablesHook = () => {
-    beforeEach('seed all data', () => helpers.seedAllTables(db));
+    beforeEach("seed all data", () => helpers.seedAllTables(db));
+  };
+  const authHeaders = { dev: {}, nonDev: {} };
+  const getAuthHeadersHook = () => {
+    beforeEach("set auth headers", async () => {
+      authHeaders.dev = await helpers.getAuthHeaders(
+        app,
+        testDev.user_name,
+        db
+      );
+      authHeaders.nonDev = await helpers.getAuthHeaders(
+        app,
+        testUser.user_name,
+        db
+      );
+    });
   };
 
-  it.skip('rejects unauthorized user', () => {
-    helpers.seedUsers(db);
+  it("rejects unauthorized user", () => {
+    return supertest(app).get(COMMENTS_EP).expect(401);
   });
 
-  describe.skip(`ENDPOINT: '/comments'`, () => {
-    context.skip('GET', () => {
-      seedAllTablesHook();
-
-      it.skip('all comments when user is a dev', () => {});
-
-      it.skip('only user-comments when user is not a dev', () => {});
-    });
-
-    context.skip('POST', () => {
-      it.skip('throws error if missing body fields', () => {});
-
-      it.skip('throws error if bug_id is invalid', () => {});
-
-      it.skip('throws error if not a dev or correct user', () => {});
-
-      it.skip('creates a new comment entry', () => {});
-
-      it.skip('returns formatted comment', () => {});
-    });
-  });
-
-  describe.skip(`ENDPOINT: '/comments/:id'`, () => {
+  describe(`ENDPOINT: '/comments'`, () => {
     seedAllTablesHook();
+    getAuthHeadersHook();
+    context("GET", () => {
+      it("all comments when user is a dev", () => {
+        return supertest(app)
+          .get(COMMENTS_EP)
+          .set(authHeaders.dev)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.comments).to.deep.equal([
+              {
+                id: 1,
+                bugName: "bug_name2",
+                userName: "user_name2",
+                comment: "comment1",
+                createdDate: res.body.comments[0].createdDate,
+              },
+              {
+                id: 2,
+                bugName: "bug_name2",
+                userName: "user_name2",
+                comment: "comment2",
+                createdDate: res.body.comments[1].createdDate,
+              },
+              {
+                id: 3,
+                bugName: "bug_name3",
+                userName: "user_name3",
+                comment: "comment3",
+                createdDate: res.body.comments[2].createdDate,
+              },
+              {
+                id: 4,
+                bugName: "bug_name2",
+                userName: "user_name1",
+                comment: "comment4",
+                createdDate: res.body.comments[3].createdDate,
+              },
+            ]);
+          });
+      });
 
-    it.skip('throws error if bug_id is invalid', () => {});
-
-    it.skip('throws error if not a dev or correct user', () => {});
-
-    context.skip('GET', () => {
-      it.skip('formatted comment by id', () => {});
+      it("only user-comments when user is not a dev", () => {
+        return supertest(app)
+          .get(COMMENTS_EP)
+          .set(authHeaders.nonDev)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.comments).to.deep.equal([
+              {
+                id: 1,
+                bugName: "bug_name2",
+                userName: "user_name2",
+                comment: "comment1",
+                createdDate: res.body.comments[0].createdDate,
+              },
+              {
+                id: 2,
+                bugName: "bug_name2",
+                userName: "user_name2",
+                comment: "comment2",
+                createdDate: res.body.comments[1].createdDate,
+              },
+            ]);
+          });
+      });
     });
 
-    context.skip('PATCH', () => {
-      it.skip('successfully updates comment', () => {});
+    context("POST", () => {
+      it("throws error if missing body fields", () => {
+        return supertest(app)
+          .post(COMMENTS_EP)
+          .set(authHeaders.nonDev)
+          .send({
+            bug_id: 2,
+          })
+          .expect(400)
+          .expect((res) => {
+            expect(res.body).to.deep.equal({
+              error: "Missing 'user_name' in request body",
+            });
+          });
+      });
 
-      it.skip('returns message and formatted updated comment', () => {});
-    });
+      it("throws error if bug_id is invalid", () => {
+        return supertest(app)
+          .post(COMMENTS_EP)
+          .set(authHeaders.nonDev)
+          .send({
+            id: 5,
+            bug_id: 6,
+            user_name: "user_name2",
+            comment: "new Comment",
+          })
+          .expect(500);
+      });
 
-    context.skip('DELETE', () => {
-      it.skip('successfully deletes comment', () => {});
+      it("throws error if not a dev or correct user", () => {
+        return supertest(app)
+          .post(COMMENTS_EP)
+          .set(authHeaders.nonDev)
+          .send({
+            id: 5,
+            bug_id: 1,
+            user_name: "user_name2",
+            comment: "new Comment",
+          })
+          .expect(401)
+          .expect((res) => {
+            expect(res.body).to.eql({
+              error: "Bug not found/unauthorized comment query",
+            });
+          });
+      });
 
-      it.skip('returns message and formatted deleted comment', () => {});
+      it("creates a new comment entry", () => {
+        return supertest(app)
+          .post(COMMENTS_EP)
+          .set(authHeaders.nonDev)
+          .send({
+            id: 5,
+            bug_id: 2,
+            user_name: "user_name2",
+            comment: "new Comment",
+          })
+          .expect(200);
+      });
+
+      it("returns formatted comment", () => {
+        return supertest(app)
+          .post(COMMENTS_EP)
+          .set(authHeaders.nonDev)
+          .send({
+            id: 5,
+            bug_id: 2,
+            user_name: "user_name2",
+            comment: "new Comment",
+          })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).to.eql({
+              newComment: {
+                id: 5,
+                bugName: "bug_name2",
+                userName: "user_name2",
+                comment: "new Comment",
+                createdDate: res.body.newComment.createdDate,
+              },
+            });
+          });
+      });
     });
   });
 
-  describe.skip('ENDPOINT: filter routes', () => {
-    const filterRoutes = [
-      '/comments/bug/:bugId',
-      '/comments/user/:userName',
-    ];
+  describe(`ENDPOINT: '/comments/:id'`, () => {
+    seedAllTablesHook();
+    getAuthHeadersHook();
+
+    it("throws error if bug_id is invalid", () => {
+      return supertest(app)
+        .get(`/api/comments/5`)
+        .set(authHeaders.nonDev)
+        .expect(500);
+    });
+
+    it("throws error if not a dev or correct user", () => {
+      return supertest(app)
+        .get(`/api/comments/4`)
+        .set(authHeaders.nonDev)
+        .expect(401)
+        .expect((res) => {
+          expect(res.body).to.eql({
+            error: "Bug not found/unauthorized comment query",
+          });
+        });
+    });
+
+    context("GET", () => {
+      it("formatted comment by id", () => {
+        return supertest(app)
+          .get(`/api/comments/2`)
+          .set(authHeaders.nonDev)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).to.eql({
+              id: 2,
+              bugName: "bug_name2",
+              userName: "user_name2",
+              comment: "comment2",
+              createdDate: res.body.createdDate,
+            });
+          });
+      });
+    });
+
+    // future feature tests!
+    context.skip("PATCH", () => {
+      it.skip("successfully updates comment", () => {});
+
+      it.skip("returns message and formatted updated comment", () => {});
+    });
+
+    // future feature tests!
+    context.skip("DELETE", () => {
+      it.skip("successfully deletes comment", () => {});
+
+      it.skip("returns message and formatted deleted comment", () => {});
+    });
+  });
+
+  // future feature tests!
+  describe.skip("ENDPOINT: filter routes", () => {
+    const filterRoutes = ["/comments/bug/:bugId", "/comments/user/:userName"];
 
     filterRoutes.forEach((route) => {
       context.skip(`GET '${route}'`, () => {
@@ -84,23 +268,23 @@ describe.skip('Route: Comment-Thread router', () => {
         let filtComments;
         let notFoundMessage;
         switch (route) {
-          case '/bug/:bugId':
+          case "/bug/:bugId":
             filtComments = null;
-            notFoundMessage = 'No comments found for bug:';
+            notFoundMessage = "No comments found for bug:";
             break;
 
-          case '/user/:userName':
+          case "/user/:userName":
             filtComments = null;
-            notFoundMessage = 'No comments found for user:';
+            notFoundMessage = "No comments found for user:";
             break;
 
           default:
             break;
         }
 
-        it.skip('returns filtered comments list', () => {});
+        it.skip("returns filtered comments list", () => {});
 
-        it.skip('returns a message if there are no comments', () => {});
+        it.skip("returns a message if there are no comments", () => {});
       });
     });
   });
